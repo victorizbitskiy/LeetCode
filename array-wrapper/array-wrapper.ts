d7f80b95702aed8fdd49bd05f7fae332429e74fd
Victor Izbitskiy <62376906+victorizbitskiy@users.noreply.github.com>
2023-05-27 16:25:39 +0300

    Time: 68 ms (32.47%) | Memory: 45 MB (61.86%) - LeetSync

--- array-wrapper/array-wrapper.ts
+++ array-wrapper/array-wrapper.ts
@@ -18,6 +18,23 @@ class ArrayWrapper {
     }
 };
 
+// or like this:
+// class ArrayWrapper {
+//   private nums: number[];
+
+//   constructor(nums: number[]) {
+//     this.nums = nums;
+//   }
+
+//   valueOf(): number {
+//     return this.nums.reduce((acc, curr) => acc + curr, 0);
+//   }
+
+//   toString(): string {
+//     return JSON.stringify(this.nums);
+//   }
+// }
+
 /**
  * const obj1 = new ArrayWrapper([1,2]);
  * const obj2 = new ArrayWrapper([3,4]);
