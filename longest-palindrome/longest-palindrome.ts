cfc3caeacb0dfe305981c7b0bcfb12a4751e788e
Victor Izbitskiy <62376906+victorizbitskiy@users.noreply.github.com>
2023-06-12 20:57:32 +0300

    Update longest-palindrome.ts
    remove semicolons

--- longest-palindrome/longest-palindrome.ts
+++ longest-palindrome/longest-palindrome.ts
@@ -1,27 +1,27 @@
 function longestPalindrome(s: string): number {
-  let result = 0;
+  let result = 0
 
-  const charFreq = new Map<string, number>();
+  const charFreq = new Map<string, number>()
 
   for (const c of s) {
     const currentFreq = charFreq.get(c);
     if (!currentFreq) {
-      charFreq.set(c, 1);
+      charFreq.set(c, 1)
     } else {
-      charFreq.set(c, currentFreq + 1);
+      charFreq.set(c, currentFreq + 1)
     }
   }
 
-  let isThereAnyOddFrequencies = 0;
+  let isThereAnyOddFrequencies = 0
 
   for (const [i, frequency] of charFreq) {
     if (frequency % 2 === 0) {
-      result += frequency;
+      result += frequency
     } else {
-      result += frequency - 1;
-      isThereAnyOddFrequencies = 1;
+      result += frequency - 1
+      isThereAnyOddFrequencies = 1
     }
   }
 
-  return result + isThereAnyOddFrequencies;
+  return result + isThereAnyOddFrequencies
 }
