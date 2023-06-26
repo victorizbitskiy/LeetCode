7d18451852e223f2f63d024d2b5613ead7d989ac
Victor Izbitskiy <62376906+victorizbitskiy@users.noreply.github.com>
2023-06-26 16:36:26 +0300

    Time: 78 ms (62.85%) | Memory: 44 MB (99.26%) - LeetSync

---
+++ sqrtx/sqrtx.ts
@@ -0,0 +1,9 @@
+function mySqrt(x: number): number {
+    // Newton-Raphson method
+    let result = x;
+    let precision = 0.00001;
+    while (Math.abs(x - result * result) > precision) {
+        result = (result + x / result) / 2
+    }
+    return Math.floor(result)
+};
